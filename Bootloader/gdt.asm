4c9836ec7018a7629c3aa5ddad32fb6f1b22946c
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-01-19 16:34:06 -0300

    Update gdt.asm

--- Bootloader/gdt.asm
+++ Bootloader/gdt.asm
@@ -1,8 +1,10 @@
 [BITS    16]
 
 SetupGDT:
+        ;Disable interrupts
         cli
 
+        ;Load GDT Address
         lgdt    [GDTR]
 
         ;Enable Protected Mode
@@ -13,37 +15,51 @@ SetupGDT:
         ret
 
 GDT:
+        ;Not Used
         GDT.Null:
                 dd  0x00
                 dd  0x00
-        
+
+        ;Code Segment
         GDT.Code:
+                ;Segment Limit
                 dw  0xFFFF
 
+                ;Base
                 dw  0x00
                 db  0x00
 
+                ;1001 1010b (Executable, R/W, Code Segment)
                 db  0x9A
 
+                ;Additional Attributes
                 db  0xCF
 
+                ;Reserved
                 db  0x00
 
         GDT.Data:
+                ;Segment Limit
                 dw  0xFFFF
 
+                ;Base
                 dw  0x00
                 db  0x00
 
+                ;Specifies Data Segment
                 db  0x92
 
+                ;Additional Attributes
                 db  0xCF
 
+                ;Reserved
                 db  0x00
 
+;GDT Struct to Store Address and Limit
 GDTR:
         dw GDTR - GDT - 1
         dd GDT
 
+;Segments definitions
 CODESEG equ GDT.Code - GDT
 DATASEG equ GDT.Data - GDT
