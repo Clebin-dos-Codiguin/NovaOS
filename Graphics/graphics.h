923a8d2593525999a256c5fb4274a4c1c122f4ea
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2025-01-20 13:21:23 -0300

    Update graphics.h

--- Graphics/graphics.h
+++ Graphics/graphics.h
@@ -6,7 +6,6 @@
 void SetPixel(int x, int y, BYTE color);
 BYTE GetPixel(int x, int y);
 void ClearScreen(void);
-void DrawBootScr(void);
 
 void StretchBlt(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
 void DrawRect(int x, int y, int w, int h, BYTE color);
